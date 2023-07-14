4c20e7a32c11fd36089de80d8c5a0fabfc2ec3fe
Haoliang Xue <hx2@sanger.ac.uk>
2023-07-14 15:00:33 +0100

    b2_Kmerator-Reindeer_vs_Kallisto.R

--- Kallisto-validation/a_tximport.R
+++ Kallisto-validation/a_tximport.R
@@ -1,30 +1,29 @@
 rm(list = ls())
 
 library(tximport)
-library(magrittr)
+library(stringr)
 library(GenomicFeatures)
 
-cmdArgs <- commandArgs(trailingOnly = T)
-gtf.path <- cmdArgs[1]
-kallisto.dir <- cmdArgs[2]
-out.dir <- cmdArgs[3]
-samples <- cmdArgs[4 : length(cmdArgs)]
-# gtf.path <- "/store/EQUIPES/SSFA/MEMBERS/haoliang.xue/reindeer_appli/data/Homo_sapiens.GRCh38.99.gtf"
-# kallisto.dir <- "/store/EQUIPES/SSFA/Data/CCLE/kallisto-ensembl99"
 
+WKDIR <- "~/Projects/working-bench/ReindeerAppli/"
+GTFPATH <- paste0(WKDIR, "data/Homo_sapiens.GRCh38.108.gtf")
+KALLISTODIR <- paste0(WKDIR, "data/kallisto-ensembl108/")
+
+samples <- list.files(KALLISTODIR) %>%
+    str_subset(pattern = "seqc")
 print(paste("Sample number: ", length(samples)))
 
-abundance.files <- file.path(paste0(kallisto.dir), samples, "abundance.h5")
+abundance.files <- paste0(paste0(KALLISTODIR), samples, "/abundance.h5")
 names(abundance.files) <- samples
 
-txdb <- makeTxDbFromGFF(file = gtf.path)
+txdb <- makeTxDbFromGFF(file = GTFPATH)
 tx2gene <- select(txdb, keys(txdb, keytype = "TXNAME"), "GENEID", "TXNAME")
     
 gene.kallisto <- tximport(files = abundance.files, type = "kallisto", 
                           txIn = TRUE, txOut = FALSE, tx2gene = tx2gene, ignoreTxVersion = T)
 gene.counts <- as.data.frame(gene.kallisto$counts)
-write.table(gene.counts, file = paste0(out.dir, "/gene-counts-tximport.tsv"), 
+write.table(gene.counts, file = paste0(KALLISTODIR, "gene-counts-tximport.tsv"), 
             col.names = T, row.names = T, sep = "\t", quote = F)    
 gene.abund <- as.data.frame(gene.kallisto$abundance)
-write.table(gene.abund, file = paste0(out.dir, "/gene-abundance-tximport.tsv"), 
+write.table(gene.abund, file = paste0(KALLISTODIR, "gene-abundance-tximport.tsv"), 
             col.names = T, row.names = T, sep = "\t", quote = F)    
