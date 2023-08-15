2f2d9c2291929488ebc7ff7cf753b7f6822c8b61
Haoliang Xue <hx2@sanger.ac.uk>
2023-08-15 10:10:41 +0100

    HERV analysis

---
+++ HERV-validation/a1_Kmerator-Reindeer_vs_GroundTruth.R
@@ -0,0 +1,102 @@
+rm(list = ls())
+
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(ggpointdensity)
+library(patchwork)
+
+WKDIR <- "~/Projects/working-bench/ReindeerAppli/"
+INDIR <- paste0(WKDIR, "data/HERV1000/")
+OUTDIR <- paste0(WKDIR, "res_HERV1000/KmeratorReindeer/")
+
+if (!dir.exists(OUTDIR)) {
+    dir.create(OUTDIR, recursive = TRUE)
+    cat("Created output folder:", OUTDIR, "\n")
+}
+
+# Load and parse Taqman table
+tab.telescope <- read.table(paste0(INDIR, "HERV1000_telescope.tsv"),
+                            header = TRUE, sep = "\t") %>%
+    pivot_longer(cols = -transcript, values_to = "Counts.true", names_to = "Sample.assay")
+
+# Load and parse Reindeer table
+tab.reindeer <- read.table(paste0(INDIR, "contigs_HERV1000_on_CCLE-24-cut.out"),
+                           header = TRUE, sep = "\t") %>%
+    dplyr::mutate(seq_name = str_remove_all(seq_name, ".contig[0-9]*")) %>%
+    pivot_longer(cols = -seq_name, values_to = "Query.reindeer", names_to = "Sample.assay") 
+tab.reindeer <- aggregate(tab.reindeer$Query.reindeer,
+                          by = list(tab.reindeer$seq_name, tab.reindeer$Sample.assay),
+                          FUN = function(qs) paste0(qs, collapse = ",")) %>%
+    dplyr::rename(transcript = Group.1, Sample.assay = Group.2, Query.reindeer = x) %>%
+    dplyr::mutate(Count.reindeer = lapply(Query.reindeer, 
+                                          FUN = function(qstr) {
+                                              qs.parsed <- strsplit(qstr, split = ",") %>% unlist()
+                                              count.vect <- NULL
+                                              for (s in qs.parsed) {
+                                                  if (nchar(s) > 1) { # if the query is not a single "*"
+                                                      pos.info <- strsplit(s, split = "-|:") %>% unlist()
+                                                      b <- as.integer(pos.info[1])
+                                                      e <- as.integer(pos.info[2])
+                                                      q <- pos.info[3]
+                                                      count.vect <- c(count.vect,
+                                                                      rep(ifelse(q != "*",
+                                                                                 yes = as.integer(q),
+                                                                                 no = 0),
+                                                                          e - b + 1))
+                                                  }
+                                              }
+                                              return(count.vect)
+                                          })) %>%
+    dplyr::mutate(Mean.reindeer = lapply(Count.reindeer,
+                                         FUN = function(x) ifelse(!is.null(x), yes = mean(x), no = 0)) %>% unlist(),
+                  Median.reindeer = lapply(Count.reindeer,
+                                           FUN = function(x) ifelse(!is.null(x), yes = median(x), no = 0)) %>% unlist(),
+                  Max.reindeer = lapply(Count.reindeer,
+                                        FUN = function(x) ifelse(!is.null(x), yes = max(x), no = 0)) %>% unlist(),
+                  Sum.reindeer = lapply(Count.reindeer,
+                                        FUN = function(x) ifelse(!is.null(x), yes = sum(x), no = 0)) %>% unlist()) %>%
+    dplyr::select(-Count.reindeer)
+
+# Merge tables to compare
+cmp.tab <- merge(tab.telescope, tab.reindeer, by = c("transcript", "Sample.assay"))
+# Compute correlations
+cor.tab <- lapply(paste0(c("Mean", "Median", "Max", "Sum"), ".reindeer"),
+                  FUN = function(mthd) {
+                      data.frame(row.names = mthd,
+                                 corr.pearson = cor(cmp.tab[, "Counts.true"], cmp.tab[, mthd], method = "pearson"),
+                                 corr.spearman = cor(cmp.tab[, "Counts.true"], cmp.tab[, mthd], method = "spearman"))
+                  }) %>%
+    do.call(what = rbind)
+# Plot scatter density
+plt_lst <- lapply(paste0(c("Mean", "Median", "Max", "Sum"), ".reindeer"),
+                  FUN = function(mthd) {
+                      ggplot() +
+                          geom_pointdensity(aes(x = cmp.tab[, "Counts.true"] + 1,
+                                                y = cmp.tab[, mthd] + 1),
+                                            size = 0.1) +
+                          geom_text(aes(x = Inf, y = 1, 
+                                        label = paste0("Pearson: ", round(cor.tab[mthd, "corr.pearson"], 2), "\n",
+                                                       "Spearman: ", round(cor.tab[mthd, "corr.spearman"], 2))),
+                                    size = 5, hjust = 1, vjust = 0) +
+                          xlab("Telescope Count") +
+                          ylab(paste0(mthd)) +
+                          scale_colour_gradient(low = "#56B1F7", high = "#132B43") +
+                          scale_x_log10(breaks = c(1, 11, 101, 1001, 10001, 100001, 1000001),
+                                        labels = c(0, 10, 100, 1000, 10000, 100000, 1000000)) +
+                          scale_y_log10(breaks = c(1, 11, 101, 1001, 10001, 100001, 1000001),
+                                        labels = c(0, 10, 100, 1000, 10000, 100000, 1000000)) +
+                          theme_bw() +
+                          theme(text = element_text(size = 15), legend.position = "none")
+                  })
+plt_lst[[1]]
+
+ggsave((plt_lst[[1]] | plt_lst[[2]]) / (plt_lst[[3]] | plt_lst[[4]]),
+       filename = paste0(OUTDIR, "SupplFig_KmeratorReindeer.4metrics.png"),
+       width = 9, height = 5, dpi = 600)
+
+# Dropout by Reindeer
+all((cmp.tab$Mean.reindeer == 0) == (cmp.tab$Max.reindeer == 0))
+write.table(cmp.tab[cmp.tab$Mean.reindeer == 0 & cmp.tab$Counts.true > 0, ],
+            paste0(OUTDIR, "KmeratorReindeer_Dropped.tsv"),
+            sep = "\t", row.names = FALSE, quote = FALSE)
