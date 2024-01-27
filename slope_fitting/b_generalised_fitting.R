e1c978a52dd0868cb14a3d158be383777e56ba8e
Haoliang Xue <hx2@sanger.ac.uk>
2024-01-27 18:12:01 +0000

    added middle fitting results, corrected a bug ranking on log1p fitting

--- slope_fitting/b_generalised_fitting.R
+++ slope_fitting/b_generalised_fitting.R
@@ -18,10 +18,10 @@ cmp.tab.pos <- cmp.tab[(cmp.tab[, mthd] > 0) & (cmp.tab[, count_column] > 0), ]
 # Fit slopes by gene ID
 fit.tab <- cmp.tab.pos %>%
     dplyr::group_by(ID) %>%
-    dplyr::summarise("alpha" = lm(formula = log1p(Sum.reindeer) ~ log1p(Kallisto.counts))$coefficients[2],
-                     "beta" = lm(formula = log1p(Sum.reindeer) ~ log1p(Kallisto.counts))$coefficients[1],
-                     "R2" = summary(lm(formula = log1p(Sum.reindeer) ~ log1p(Kallisto.counts)))$adj.r.squared,
+    dplyr::summarise("slope" = lm(formula = Sum.reindeer ~ Kallisto.counts)$coefficients[2],
+                     "R2" = summary(lm(formula = Sum.reindeer ~ Kallisto.counts))$adj.r.squared,
                      "n_smp" = dplyr::n())
+write.table(fit.tab, paste0(WKDIR, "fitting_slope/fit_tab.tsv"), sep="\t", row.names = FALSE)
 
 # Figure 1: histogram of adjusted R2
 plt1 <- ggplot(na.omit(fit.tab)) +
@@ -55,6 +55,25 @@ plt_lst <- lapply(top_genes, FUN = function(gene2sel) {
 })
 ggsave(wrap_plots(plt_lst, ncol = 5),
        filename = paste0(WKDIR, "fitting_slope/top25_genes_fitted.pdf"),
+       width = 15, height = 15)
+
+# Plot best fitting cases
+ok_genes <- fit.tab.2plot$ID[order(fit.tab.2plot$R2, decreasing = TRUE)[701:725]]
+plt_lst <- lapply(ok_genes, FUN = function(gene2sel) {
+    cmp.tab.pos.sub <- cmp.tab.pos[cmp.tab.pos$ID == gene2sel, ]
+    lm.res <- lm(formula = cmp.tab.pos.sub$Sum.reindeer ~ cmp.tab.pos.sub$Kallisto.counts)
+    ggplot(cmp.tab.pos.sub) +
+        geom_point(aes(x = Kallisto.counts, y = Sum.reindeer)) +
+        geom_text(aes(x = Inf, y = 0,
+                      label = paste0("slope = ", round(lm.res$coefficients[2], 2), "\n",
+                                     "adj.R2 = ", round(summary(lm.res)$adj.r.squared, 1))),
+                  hjust = 1.1, vjust = 0.1) +
+        ggtitle(gene2sel) +
+        theme_bw() +
+        theme(text = element_text(size = 12))
+})
+ggsave(wrap_plots(plt_lst, ncol = 5),
+       filename = paste0(WKDIR, "fitting_slope/rank701to725_genes_fitted.pdf"),
        width = 15, height = 15)
 
 # Plot worst fitting cases
